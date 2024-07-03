f98a9b0e9619ea684e4aab78ed52a81ccee0902b
Ankan Mandal <ankanmandal2001@gmail.com>
2024-07-04 03:58:09 +0530

    authentication is done

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+const user=require('../models/user-model.js');
+const jwt =require('jsonwebtoken');
+
+const authentication =(req,res,next)=>{
+
+    const token=req.header("Auth");
+    if(!token){
+        res.json({loginStatus:false});
+    }else{
+        const decoded=jwt.verify(token,"^&$*$%@&^");
+        const id=decoded.userId;
+        const User=user.findById(id);
+        if(!User){
+            res.json({message:"user not found",success:false});
+        }else{
+            req.user=User;
+            next();
+        }
+    }
+
+}
+
+module.exports=authentication;
--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -5,12 +5,13 @@ const addToCart =require("../controllers/cart/addToCart.js")
 const getCartDetails =require("../controllers/cart/getCartDetails.js")
 const deleteCartItem =require("../controllers/cart/deleteCartItem.js")
 const clearCart =require("../controllers/cart/clearCart.js")
+const authentication =require("../middleware/auth.js");
 
 
-router.post('/addToCart',addToCart);
-router.get('/getCart/:id',getCartDetails);
-router.delete('/deleteItem/:id',deleteCartItem);
-router.delete('/clearCart',clearCart);
+router.post('/addToCart',authentication,addToCart);
+router.get('/getCart/:id',authentication,getCartDetails);
+router.delete('/deleteItem/:id',authentication,deleteCartItem);
+router.delete('/clearCart',authentication,clearCart);
 
 
 module.exports=router
