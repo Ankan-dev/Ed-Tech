d4452cc64d1e8432a3e9e10da4de2463bd00078d
Ankan Mandal <ankanmandal2001@gmail.com>
2024-07-14 02:07:03 +0530

    user, category, teacher, products are finally over

--- backend/controllers/products/addProducts.js
+++ backend/controllers/products/addProducts.js
@@ -1,17 +1,36 @@
 const products = require('../../models/product-model.js');
 const category_model = require('../../models/category-model.js');
+const teacherModel = require('../../models/teacher-model.js');
 
 const addProduct = async (req, res) => {
-    const { creator, category, image, description, name, price, discount } = req.body;
+    const { category, image, description, name, price, discount } = req.body;
+    const teacher = req.teacherModel;
     try {
-        const createProduct = await products.create({ creator, category, image, description, name, price, discount });
+
         const findCategory = await category_model.findById(category);
-        
-        if (findCategory) {
-            findCategory.products.push(createProduct._id);
-            await findCategory.save();
+        const findTeacher = await teacherModel.findById(teacher.id);
+
+        if (!findCategory) {
+            return res.json({
+                message: "category not found",
+                success: false
+            })
         }
 
+        if (!findTeacher) {
+            return res.json({
+                message: "Teacher not found",
+                success: false
+            })
+        }
+
+        const createProduct = await products.create({ creator: findTeacher._id, category, image, description, name, price, discount });
+
+        findTeacher.Products.push(createProduct._id);
+        await findTeacher.save({ validateBeforeSave: false });
+
+        findCategory.products.push(createProduct._id);
+        await findCategory.save({ validateBeforeSave: false });
         res.json({ message: "Product created successfully", success: true });
     } catch (error) {
         console.error(error);  // It's good practice to log errors for debugging
--- backend/routes/productsRouter.js
+++ backend/routes/productsRouter.js
@@ -1,12 +1,12 @@
 const express =require('express');
 const router=express.Router();
-
+const TeacherAuthorization=require('../middleware/TeacherAuth.js');
 const addProduct=require('../controllers/products/addProducts.js');
 const {getAllProducts,getProductsById}=require('../controllers/products/getProducts.js');
 const updateProductsById =require('../controllers/products/updateProducts.js');
 const deleteProductsById=require('../controllers/products/deleteProducts.js');
 
-router.post('/addProducts',addProduct);
+router.post('/addProducts',TeacherAuthorization,addProduct);
 router.get('/getAllProducts',getAllProducts);
 router.get('/:id',getProductsById);
 router.put('/update/:id',updateProductsById);
